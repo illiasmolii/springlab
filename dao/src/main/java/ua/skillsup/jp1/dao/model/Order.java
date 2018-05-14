c389f68eedb72e34b3f5aa8c94055dd0f56688bf
Illia Smolii <illiasmolii@gmail.com>
2018-05-14 09:23:15 +0300

    ORM: Start mapping orders.

--- dao/src/main/java/ua/skillsup/jp1/dao/model/Order.java
+++ dao/src/main/java/ua/skillsup/jp1/dao/model/Order.java
@@ -2,6 +2,15 @@ package ua.skillsup.jp1.dao.model;
 
 import java.util.Map;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,9 +18,18 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "ORDERS")
 public class Order {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@ManyToOne
+	@JoinColumn
+	@Column(name = "USER", nullable = false)
 	private User user;
+
 	private Map<Product, Integer> products;
 }
