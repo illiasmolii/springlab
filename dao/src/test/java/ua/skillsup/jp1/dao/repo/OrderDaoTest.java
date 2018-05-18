5820f5929a4cb752dfe95bbafa3dc304e4ba3f0d
Illia Smolii <illiasmolii@gmail.com>
2018-05-18 09:31:01 +0300

    ORM: Test find order by ID.

--- dao/src/test/java/ua/skillsup/jp1/dao/repo/OrderDaoTest.java
+++ dao/src/test/java/ua/skillsup/jp1/dao/repo/OrderDaoTest.java
@@ -9,10 +9,12 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -39,15 +41,12 @@ public class OrderDaoTest {
 	@Test
 	public void test_create_order() {
 		// given
-		User user = new User(null, "John", "Doe", 42);
-		entityManager.persist(user);
+		User user = createUser();
 
-		Product product1 = new Product(null, "Levis 501", BigDecimal.TEN, Category.CLOTHES,
+		Product product1 = createProduct("Levis 501", 120L, Category.CLOTHES,
 				Gender.FEMALE, Color.BLUE, Size.S, 10);
-		Product product2 = new Product(null, "Timberland", BigDecimal.TEN, Category.FOOTWEAR,
+		Product product2 = createProduct("Timberland", 145L, Category.FOOTWEAR,
 				Gender.MALE, Color.BLACK, Size.XL, 3);
-		entityManager.persist(product1);
-		entityManager.persist(product2);
 
 		Order order = new Order(user, null);
 
@@ -69,27 +68,66 @@ public class OrderDaoTest {
 
 	@Test
 	public void test_find_order_by_id() {
+		// given
+		User user = createUser();
+		Product product = createProduct("Levis 501", 100L, Category.CLOTHES,
+				Gender.FEMALE, Color.BLUE, Size.S, 10);
+
+		Order order = new Order(user, null);
+
+		Set<OrderItem> orderItems = new HashSet<>();
+		orderItems.add(
+				new OrderItem(new OrderItem.Id(null, product.getId()), order, product, 1));
+
+		order.setOrderItems(orderItems);
+
+		entityManager.persist(order);
+
+		// when
+		Order foundOrder = orderDao.findById(order.getId());
+
+		// then
+		Assert.assertEquals(order, foundOrder);
+	}
+
+	@Ignore
+	@Test
+	public void test_find_orders_by_ids() {
 
 	}
 
+	@Ignore
 	@Test
-	public void test_find_order_by_ids() {
-
-	}
-
-	@Test
+	@Sql("/sql/cleanup.sql")
 	public void test_find_all_orders() {
 		List<Order> orders = orderDao.findAll();
 		System.out.println(orders);
 	}
 
+	@Ignore
 	@Test
 	public void test_update_order() {
 
 	}
 
+	@Ignore
 	@Test
 	public void test_delete_order() {
 
+	}
+
+	private User createUser() {
+		User user = new User(null, "John", "Doe", 42);
+		entityManager.persist(user);
+		return user;
+	}
+
+	private Product createProduct(String name, Long price, Category category,
+			Gender gender, Color color, Size size, Integer remainng) {
+
+		Product product = new Product(null, name, BigDecimal.valueOf(price), category,
+				gender, color, size, remainng);
+		entityManager.persist(product);
+		return product;
 	}
 }
