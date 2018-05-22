1d874d6c619aa11a78d486cefc3e80e1fc63f74f
Illia Smolii <illiasmolii@gmail.com>
2018-05-22 09:13:15 +0300

    WEB: Configuration in progress

---
+++ web/src/main/java/ua/skillsup/jp1/initializers/ShopAppInitializer.java
@@ -0,0 +1,23 @@
+package ua.skillsup.jp1.initializers;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.XmlWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class ShopAppInitializer implements WebApplicationInitializer {
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		XmlWebApplicationContext context = new XmlWebApplicationContext();
+		context.setConfigLocation("context-web.xml");
+
+		ServletRegistration.Dynamic dispatcher =
+				servletContext.addServlet("dispatcher", new DispatcherServlet(context));
+		dispatcher.setLoadOnStartup(1);
+		dispatcher.addMapping("/");
+	}
+}
