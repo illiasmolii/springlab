ad58eb14e8f0069986b2fdd1e7c781067f7960c9
Illia Smolii <illiasmolii@gmail.com>
2018-05-23 08:44:30 +0300

    WEB: Logging.

--- web/src/main/java/ua/skillsup/jp1/controllers/UserController.java
+++ web/src/main/java/ua/skillsup/jp1/controllers/UserController.java
@@ -8,11 +8,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import lombok.extern.slf4j.Slf4j;
 import ua.skillsup.jp1.services.UserService;
 import ua.skillsup.jp1.services.dto.UserDto;
 
 @Controller
 @RequestMapping("/users")
+@Slf4j
 public class UserController {
 
 	private final UserService userService;
@@ -25,6 +27,7 @@ public class UserController {
 	@RequestMapping(value = "/all", method = RequestMethod.GET, produces = "application/json")
 	@ResponseBody
 	public List<UserDto> getAll() {
+		log.info("Find all users");
 		return userService.findAll();
 	}
 }
