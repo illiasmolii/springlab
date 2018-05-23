036a07922463ce76644ddc343378d0385de6e42b
Illia Smolii <illiasmolii@gmail.com>
2018-05-23 08:34:28 +0300

    WEB: JSON response.

--- web/src/main/java/ua/skillsup/jp1/controllers/UserController.java
+++ web/src/main/java/ua/skillsup/jp1/controllers/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import ua.skillsup.jp1.services.UserService;
 import ua.skillsup.jp1.services.dto.UserDto;
@@ -21,7 +22,8 @@ public class UserController {
 		this.userService = userService;
 	}
 
-	@RequestMapping(value = "/all", method = RequestMethod.GET)
+	@RequestMapping(value = "/all", method = RequestMethod.GET, produces = "application/json")
+	@ResponseBody
 	public List<UserDto> getAll() {
 		return userService.findAll();
 	}
